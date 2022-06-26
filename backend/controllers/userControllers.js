a357ff80e61d3669d3c25d956b96ba21fef9e1a8
Siddharth Saurav <sid.saurav11@gmail.com>
2022-06-27 01:21:01 +0530

    add auth middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,19 +1,19 @@
-const asyncHandler = require("express-async-handler");
-const User = require("../models/userModel");
-const generateToken = require("../config/generateToken");
-const bcrypt = require("bcryptjs");
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+const generateToken = require('../config/generateToken')
+const bcrypt = require('bcryptjs')
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password, pic } = req.body;
+    const { name, email, password, pic } = req.body
 
     if (!name || !email || !password) {
-        res.status(400).json("please enter all fields");
+        res.status(400).json('please enter all fields')
     }
 
-    const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email })
 
     if (userExists) {
-        res.status(400).json("user already exits");
+        res.status(400).json('user already exits')
     }
 
     const user = await User.create({
@@ -21,7 +21,7 @@ const registerUser = asyncHandler(async (req, res) => {
         email,
         password,
         pic,
-    });
+    })
 
     if (user) {
         res.status(201).json({
@@ -31,20 +31,20 @@ const registerUser = asyncHandler(async (req, res) => {
             isAdmin: user.isAdmin,
             pic: user.pic,
             token: generateToken(user._id),
-        });
+        })
     } else {
-        res.status(400);
-        throw new Error("User not found");
+        res.status(400)
+        throw new Error('User not found')
     }
-});
+})
 
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
     if (!user) {
-        res.status(401).json("Invalid Email or Password");
+        res.status(401).json('Invalid Email or Password')
     }
-    const validate = bcrypt.compareSync(password, user.password);
+    const validate = bcrypt.compareSync(password, user.password)
 
     if (user && validate) {
         res.status(201).json({
@@ -54,10 +54,24 @@ const authUser = asyncHandler(async (req, res) => {
             isAdmin: user.isAdmin,
             pic: user.pic,
             token: generateToken(user._id),
-        });
+        })
     } else {
-        res.status(401).json("Invalid Email or Password");
+        res.status(401).json('Invalid Email or Password')
     }
-});
+})
 
-module.exports = { registerUser, authUser };
+const allUsers = async (req, res) => {
+    const keyword = req.query.search
+        ? {
+              $or: [
+                  { name: { $regex: req.query.search, $options: 'i' } },
+                  { email: { $regex: req.query.search, $options: 'i' } },
+              ],
+          }
+        : {}
+
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } })
+    res.send(users)
+}
+
+module.exports = { registerUser, authUser, allUsers }
--- backend/routers/userRouters.js
+++ backend/routers/userRouters.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, authUser } = require('../controllers/userControllers')
+const {
+    registerUser,
+    authUser,
+    allUsers,
+} = require('../controllers/userControllers')
+const { protect } = require('../middleware/authMiddleware')
 
-router
-    .route('/')
-    .post(registerUser)
-    .get((req, res) => {
-        res.json('Hi')
-    })
+router.route('/').post(registerUser).get(protect, allUsers)
 router.route('/login').post(authUser)
 
 module.exports = router
