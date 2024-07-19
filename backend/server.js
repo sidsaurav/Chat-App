9a6a71c2a693b80293d679df249e7c173ceae6fc
Siddharth Saurav <sid.saurav11@gmail.com>
2024-07-19 18:44:31 +0530

    add redis scaling

--- backend/server.js
+++ backend/server.js
@@ -1,71 +1,77 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
-const app = express()
-const userRouter = require('./routers/userRouter')
-const chatRouter = require('./routers/chatRouter')
-const messageRouter = require('./routers/messageRouter')
-const cors = require('cors')
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const app = express();
+const userRouter = require("./routers/userRouter");
+const chatRouter = require("./routers/chatRouter");
+const messageRouter = require("./routers/messageRouter");
+const cors = require("cors");
 
-dotenv.config()
-app.use(express.json())
-app.use(cors())
+dotenv.config();
+app.use(express.json());
+app.use(cors());
 
-connectDB()
+connectDB();
 
 const server = app.listen(process.env.PORT, () =>
-  console.log('Server is running on port 5000')
-)
+  console.log("Server is running on port 5000")
+);
 app.use((req, res, next) => {
-  console.log(req.originalUrl)
-  console.log('Request Made')
-  next()
-})
+  console.log(req.originalUrl);
+  console.log("Request Made");
+  next();
+});
 
-app.use('/api/user', userRouter)
-app.use('/api/chat', chatRouter)
-app.use('/api/message', messageRouter)
-app.get('/', (req, res) => {
-  res.send('API is running :)')
-})
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRouter);
+app.get("/", (req, res) => {
+  res.send("API is running :)");
+});
 
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: '*',
+    origin: "*",
   },
-})
+});
 
-io.on('connection', (socket) => {
-  console.log('connected to socket.io')
+const pubClient = createClient({ url: "redis://your-redis-url" });
+const subClient = pubClient.duplicate();
 
-  socket.on('setup', (userData) => {
-    socket.join(userData._id)
-    socket.emit('connected')
-  })
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+  io.adapter(createAdapter(pubClient, subClient));
 
-  socket.on('join chat', (room) => {
-    socket.join(room)
-    console.log('User joined room: ', room)
-  })
+  io.on("connection", (socket) => {
+    console.log("connected to socket.io");
 
-  socket.on('typing', (room) => socket.in(room).emit('typing'))
-  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'))
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
+    });
 
-  socket.on('new message', (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User joined room: ", room);
+    });
 
-    if (!chat.users) return console.log('chat.users not defined')
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return
+    socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
 
-      socket.in(user._id).emit('message recieved', newMessageRecieved)
-    })
-  })
+      if (!chat.users) return console.log("chat.users not defined");
 
-  socket.off('setup', () => {
-    console.log('USER DISCONNECTED')
-    socket.leave(userData._id)
-  })
-})
+      chat.users.forEach((user) => {
+        if (user._id == newMessageRecieved.sender._id) return;
+
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("USER DISCONNECTED");
+    });
+  });
+});
